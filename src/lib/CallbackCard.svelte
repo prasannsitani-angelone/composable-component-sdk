836952c1081c283e9bc9b7d835d3b1a1f34abb45
prasannsitani-angelone <prasann.sitani@angelbroking.com>
2023-09-13 14:25:46 +0530

    feat: added callback card

---
+++ src/lib/CallbackCard.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { Card, Bottomsheet } from '../lib';
+
+	let open = false;
+
+	const handleOpen = () => {
+		open = true;
+	};
+
+	const handleClose = () => {
+		open = false;
+	};
+</script>
+
+<main>
+	<Card on:open={handleOpen} />
+	<Bottomsheet {open} on:close={handleClose} />
+</main>
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,4 @@
 // Reexport your entry components here
 export { default as Card } from './Card.svelte';
 export { default as Bottomsheet } from './Bottomsheet.svelte';
+export { default as CallbackCard } from './CallbackCard.svelte';
