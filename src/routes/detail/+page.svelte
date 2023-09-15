dfe276c770fe16483f9d359a95b68e907a5863e5
prasannsitani-angelone <prasann.sitani@angelbroking.com>
2023-09-15 11:13:34 +0530

    fix: navigation changes

--- src/routes/detail/+page.svelte
+++ src/routes/detail/+page.svelte
@@ -1,5 +1,14 @@
+<script>
+	import { goto } from '$app/navigation';
+
+	function handleClick() {
+		goto('http://localhost:3000/about');
+	}
+</script>
+
 <main>
 	<div>
 		<h3>Detail Page</h3>
+		<button on:click={handleClick}> Click Me </button>
 	</div>
 </main>
